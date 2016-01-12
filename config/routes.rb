81fbbdce700e66d7947e2e15fc009fd831f5a581
Shlok Srivastava <shlok.srivastava15@gmail.com>
2016-01-12 02:35:42 +0000

    Added page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'page/home'
+
+  get 'page/details'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'page#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
